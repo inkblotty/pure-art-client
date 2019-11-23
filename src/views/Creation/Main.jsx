c291dcd298a0724c13e3e2f1e84d0a39156dadfa
Katie Foster <fosterkatiea@gmail.com>
2019-11-22 22:18:45 -0600

    tweak

--- src/views/Creation/Main.jsx
+++ src/views/Creation/Main.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 class Creation extends React.Component {
 
     render() {
-        return <div>Creation</div>
+        return (
+            <div>
+                Creation: all three modes with a big canvas
+                - In progress (editable)
+                - Pending (can review which transactions are done, which ones are not)
+                - Complete (can look at the art and transactions that went into it)
+            </div>
+        );
     }
 }
 
